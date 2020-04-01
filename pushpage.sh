85dc7cf141e0bfe7273c5101d5c88ec2f3c0d135
Nadia Bernhardt <nadia_bernhardt@intuit.com>
2020-03-31 19:26:30 -0700

    new page

--- pushpage.sh
+++ pushpage.sh
@@ -1,3 +1,6 @@
 npm run build
 git cm -am "new page"
-git subtree push --prefix dist origin gh-pages --force
+#git subtree push --prefix dist origin gh-pages --force
+git subtree split --prefix dist -b gh-pages
+git push -f origin gh-pages:gh-pages
+git branch -D gh-pages
